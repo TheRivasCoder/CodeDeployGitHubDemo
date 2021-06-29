7d3183dca3ade6c00c310573d92b1700753bcc79
Julian Rivas <chrisrivasstudio@gmail.com>
2021-06-29 16:56:01 -0700

    Change app message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'This is a Test!'
+        return 'This is a Test and another test!'
 
 if __name__ == "__main__":
         app.run()
