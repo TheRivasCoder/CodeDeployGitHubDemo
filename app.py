3c867e6ae4d66f8eb4dd3b1297c38db839412510
Julian Rivas <chrisrivasstudio@gmail.com>
2021-06-29 17:22:23 -0700

    Change app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'This is a Test and another test to stop!'
+        return 'This is a Test!'
 
 if __name__ == "__main__":
         app.run()
