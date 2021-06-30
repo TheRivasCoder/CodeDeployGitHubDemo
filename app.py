d2a3b8667c35b0db582c614353f6c4c9fcee29b0
Julian Rivas <chrisrivasstudio@gmail.com>
2021-06-29 17:03:07 -0700

    Test Deploy when server is stoped

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-        return 'This is a Test and another test!'
+        return 'This is a Test and another test to stop!'
 
 if __name__ == "__main__":
         app.run()
